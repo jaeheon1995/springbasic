f3d4b8a07c14469dce763334097ad01fb794a94a
jaeheon jeong <huchehuche@naver.com>
2024-08-26 17:32:24 +0900

    24082618

--- src/main/java/com/example/springbasic/SpringbasicApplication.java
+++ src/main/java/com/example/springbasic/SpringbasicApplication.java
@@ -4,18 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // import com.example.springbasic.DTO.lombok;
-
 @SpringBootApplication
 public class SpringbasicApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbasicApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbasicApplication.class, args);
+    }
 
-	// void lombokExample(){
-	// 	lombok lombok = new lombok("a","b","c";);
-	// 	lombok.getField1();
-	// 	lombok.setField3(field3:null);
-
-	// }
+    // void lombokExample(){
+    // 	lombok lombok = new lombok("a","b","c";);
+    // 	lombok.getField1();
+    // 	lombok.setField3(field3:null);
+    // }
 }
